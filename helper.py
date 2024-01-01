8eb1caf6745388899554a1e27627cfe75d13beed
dichobrown <152457683+dichobrown@users.noreply.github.com>
2024-01-01 09:50:37 +0800

    Create helper.py
    open target.py

---
+++ helper.py
@@ -0,0 +1,15 @@
+import random
+
+
+def random_phone_number():
+    prefix = [57, 12, 13, 21, 95, 18]
+    
+    start_digits = "8" + str(randomalloperator.choice(prefix))
+    middle_digits = str(random.randint(10000000, 99999999))
+    random_number = start_digits + middle_digits
+
+    return str(random_number)
+
+
+def random_single_number():
+    return str(random.randint(0, 9))
